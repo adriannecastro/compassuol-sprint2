4b49bd3109d13bba7eb06d6cb4d880e3f67f051e
Adrianne Castro <45113260+adriannecastro@users.noreply.github.com>
2022-06-18 22:55:24 -0300

    Adding scripts

--- Scripts/question1.sql
+++ Scripts/question1.sql
@@ -1,1 +1,12 @@
-SELECT * FROM Products.tbProducts;
+CREATE SCHEMA `Products` DEFAULT CHARACTER SET utf8 ;
+
+USE Products;
+
+CREATE TABLE `tbProducts` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(45) NOT NULL,
+    `description` VARCHAR(45) NOT NULL,
+    `quantity` INT NOT NULL,
+    `price` DOUBLE NOT NULL,
+    PRIMARY KEY (`id`));
+)
